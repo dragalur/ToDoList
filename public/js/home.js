7fa30ae9a6a91d54792d0a66613d310525195fb3
Daniel Hlazunow <tresdio1@gmail.com>
2021-06-30 23:28:00 +0300

    Fix bag with adding card. Refactor script for home page.

--- public/js/home.js
+++ public/js/home.js
@@ -1,45 +1,51 @@
-const addB = document.getElementById('add-item');
-const addBlock = document.getElementById('add-block');
-const closeBt = document.getElementById('close');
+(function () {
+   const addB = document.getElementById('add-item');
+   const addBlock = document.getElementById('add-block');
+   const closeBt = document.getElementById('close');
 
-addB.addEventListener('click', function () {
-   addB.style.transform = 'rotateX(180deg)';
-   addBlock.style.transform = 'rotateX(0)';
-});
+   addB.addEventListener('click', function () {
+      addB.style.transform = 'rotateX(180deg)';
+      addBlock.style.transform = 'rotateX(0)';
+   });
 
-closeBt.addEventListener('click', function () {
-   addB.style.transform = 'rotateX(0)';
-   addBlock.style.transform = 'rotateX(180deg)';
-});
+   closeBt.addEventListener('click', function () {
+      addB.style.transform = 'rotateX(0)';
+      addBlock.style.transform = 'rotateX(180deg)';
+   });
 
-const buttonAddCard = document.getElementById('bt');
+   const buttonAddCard = document.getElementById('bt');
 
-buttonAddCard.addEventListener('click', function () {
-   fetch('/home', {
-      method: 'POST',
-      body: JSON.stringify({ name: document.getElementById('name').value }),
-      headers: { 'Content-Type': 'application/json' }
-   })
-      .then(response => response.json())
-      .then(data => {
-         // if (Array.isArray(data)) createCard(data[data.length - 1].name);
-         !!Array.isArray(data) && createCard(data[data.length - 1].name);
-         errorText(data.error);
-      });
-});
+   buttonAddCard.addEventListener('click', function () {
+      const name = document.getElementById('name').value;
+      if (name.trim() === '') {
+         errorText('Please enter name');
+         return;
+      }
+
+      fetch('/home', {
+         method: 'POST',
+         body: JSON.stringify({ name }),
+         headers: { 'Content-Type': 'application/json' }
+      })
+         .then(response => response.json())
+         .then(data => {
+            !!Array.isArray(data) && createCard(data[data.length - 1].name);
+            errorText(data.error);
+         });
+   });
+})();
 
 function createCard(name) {
    const form = `<div class="item-card item">
    <a href="./table?table=${name}" class="link">${name}</a>
       </div>`;
    let div = document.createRange().createContextualFragment(form);
-   const lastDiv = document.getElementsByClassName('item-card');
-   //FIXME: change on card create table, because it doesn't work when list is empty'
-   lastDiv[lastDiv.length - 1].parentNode.insertBefore(
-      div,
-      lastDiv[lastDiv.length - 1].nextSibling
-   );
+
+   const lastDiv = document.getElementById('add');
+
+   lastDiv.parentNode.insertBefore(div, lastDiv);
    document.getElementById('name').value = '';
+   document.getElementById('close').click();
 }
 
 function errorText(text = null) {
